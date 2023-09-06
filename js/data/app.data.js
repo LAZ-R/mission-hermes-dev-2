d61312a20e634c77c261154671b3acf3563cd9e1
LAZ-R <crazydrawer62@gmail.com>
2023-09-06 16:11:36 +0200

    v 0.0.2 : fix icone et couleur thème

--- js/data/app.data.js
+++ js/data/app.data.js
@@ -1,3 +1,3 @@
 export const APP_NAME = "Mission Hermès Dev 2"
-export const APP_VERSION_NUMBER = '0.0.1';
-// v 0.0.1 : populate repo
+export const APP_VERSION_NUMBER = '0.0.2';
+// v 0.0.2 : fix icone et couleur thème
